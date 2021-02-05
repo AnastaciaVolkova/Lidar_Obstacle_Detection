228b8312678c0b435003613b6d32718ad8a23a53
AnastaciaVolkova <polarbolide@gmail.com>
2021-02-05 23:49:59 +0100

    feat: Create insert function for kd-Tree

--- src/quiz/cluster/kdtree.h
+++ src/quiz/cluster/kdtree.h
@@ -37,19 +37,31 @@ struct KdTree
 
 	void insert(std::vector<float> point, int id)
 	{
-    InsertHelper(&root, point, id, 0);
-	}
+		InsertHelper(&root, point, id, 0);
+	};
 
+	void SearchHelper(Node* node, int depth, float distanceTol, std::vector<float>& target, std::vector<int>& ids){
+		if (node == NULL)
+			return;
+
+		if (abs(node->point[0]-target[0])<distanceTol && abs(node->point[1]-target[1])<distanceTol)
+			ids.push_back(node->id);
+		
+		int x_y = depth%2;
+		Node* nodes[] = {node->right, node->left};
+		bool is_left = target[x_y] < node->point[x_y];
+		
+		if (abs(nodes[!is_left]->point[0]-target[0])<distanceTol && abs(nodes[!is_left]->point[1]-target[1])<distanceTol)
+			ids.push_back(nodes[!is_left]->id);
+
+		SearchHelper(nodes[is_left], depth+1, distanceTol, target, ids);
+	};
+	
 	// return a list of point ids in the tree that are within distance of target
 	std::vector<int> search(std::vector<float> target, float distanceTol)
 	{
 		std::vector<int> ids;
+		SearchHelper(root, 0, distanceTol, target, ids);
 		return ids;
 	}
-	
-
 };
-
-
-
-
