ddb331ec3932d18eb880a7a83f9a86c3df322fbc
AnastaciaVolkova <polarbolide@gmail.com>
2021-02-14 01:02:54 +0100

    feat: Enlarge kd-tree to k dimensions

--- src/quiz/cluster/kdtree.h
+++ src/quiz/cluster/kdtree.h
@@ -26,9 +26,9 @@ struct KdTree
 
   void InsertHelper(Node** node, std::vector<float>& point, int id, int depth){
       if (*node == NULL){
-        *node = new Node(point, id);      
+        *node = new Node(point, id);
       } else {
-        int x_y = (depth%2);
+        int x_y = (depth%point.size());
         Node** next_node = (point[x_y] < (*node)->point[x_y])? &((*node)->left): &((*node)->right);
         InsertHelper(next_node, point, id, depth+1);
       }
@@ -45,19 +45,22 @@ struct KdTree
 
 		// Check if current point is inside target box.
 		if (abs(node->point[0]-target[0])<distanceTol && abs(node->point[1]-target[1])<distanceTol) {
-			float distance = sqrt(pow(node->point[0] - target[0], 2) + pow(node->point[1] - target[1], 2));
+			float distance = 0;
+			for (int i = 0; i < target.size(); i++)
+				distance += pow(node->point[i] - target[i], 2);
+			distance = sqrt(distance);
 			if (distance <= distanceTol)
 				ids.push_back(node->id);
 		}
-		
-		int x_y = depth%2;
-		
+
+		int x_y = depth%target.size();
+
 		if (target[x_y]-distanceTol < node->point[x_y])
 			SearchHelper(node->left, depth+1, distanceTol, target, ids);
 		if (target[x_y]+distanceTol > node->point[x_y])
 			SearchHelper(node->right, depth+1, distanceTol, target, ids);
 	};
-	
+
 	// return a list of point ids in the tree that are within distance of target
 	std::vector<int> search(std::vector<float> target, float distanceTol)
 	{
